05fcff10d4230f1a26974fd3437d5992ce0d0ac1
vladimirovicp <vladimirovicp70@gmail.com>
2023-01-20 17:07:44 +0400

    add: поддержка markdown

--- webpack.config.js
+++ webpack.config.js
@@ -2,12 +2,14 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 const FileManagerPlugin = require('filemanager-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const ImageMinimizerPlugin = require('image-minimizer-webpack-plugin');
 
 module.exports = {
     entry: path.join(__dirname, 'src', 'index.js'),
     output: {
         path: path.join(__dirname, 'dist'),
         filename: 'index.[contenthash].js',
+        assetModuleFilename: path.join('images', '[name].[contenthash][ext]'),
     },
     module: {
         rules: [
@@ -28,6 +30,17 @@ module.exports = {
                     'postcss-loader',
                     'sass-loader'
                 ],
+            },
+            {
+                test: /\.(png|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+            },
+            {
+                test: /\.svg$/,
+                type: 'asset/resource',
+                generator: {
+                    filename: path.join('icons', '[name].[contenthash][ext]'),
+                },
             },
         ],
     },
@@ -52,5 +65,22 @@ module.exports = {
         watchFiles: path.join(__dirname, 'src'),
         port: 9000,
     },
+   optimization: {
+     minimizer: [
+         new ImageMinimizerPlugin({
+             minimizer: {
+                 implementation: ImageMinimizerPlugin.imageminMinify,
+                 options: {
+                     plugins: [
+                         ['gifsicle', { interlaced: true }],
+                         ['jpegtran', { progressive: true }],
+                         ['optipng', { optimizationLevel: 5 }],
+                         ['svgo', { name: 'preset-default' }],
+                    ],
+                },
+            },
+         }),
+     ],
+   },
 
 };
